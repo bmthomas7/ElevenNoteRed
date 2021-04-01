bef55f80b842d7cc8f8a254110a3a8ba26944158
Ben Thomas <bmthomas7@gmail.com>
2021-04-01 18:33:37 -0400

    add to create inside of notecontroller

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,8 @@ namespace ElevenNote.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
+            var userid = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userid);
             var model = new NoteListItem[0];
             return View(model);
         }
@@ -31,11 +35,20 @@ namespace ElevenNote.WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-
+                return View(model);
             }
-            return View(model);
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+
+            var service = new NoteService(userId);
+
+
+            service.CreateNote(model);
+
+
+            return RedirectToAction("Index");
         }
     }
 }
